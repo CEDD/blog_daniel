cf6596ebf9b28f723643bc37a4583b97a2fe2c82
CEDD <danieldavid83@gmail.com>
2017-02-23 11:01:15 -0500

    Variables desde el template

--- blog_daniel/mainapp/views.py
+++ blog_daniel/mainapp/views.py
@@ -2,6 +2,20 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+
+#def index(request):
+#	return render(request,"index.html")
+
+class Entry():
+	def  __init__(self,title,body):
+		self.title = title
+		self.body = body
+
 def index(request):
-	return render(request,"index.html")
+	entrys = [
+		Entry("1er Post","1era Linea"),
+		Entry("2do Post","2da Linea")
+	]
+	entrys_total = len(entrys)
+	return render(request,"index.html",{'entrys':entrys,'entrys_total':entrys_total})
 
